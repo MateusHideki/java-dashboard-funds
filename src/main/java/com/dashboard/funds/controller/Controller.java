16965bba6a6f0739d5cdebfc43c438ba47b113c6
unknown <hidekisimote@hotmail.com.br>
2022-04-24 11:17:56 -0300

    alterando cors

--- src/main/java/com/dashboard/funds/controller/Controller.java
+++ src/main/java/com/dashboard/funds/controller/Controller.java
@@ -3,6 +3,7 @@ package com.dashboard.funds.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.dashboard.funds.model.FundRequest;
 import com.dashboard.funds.model.FundResponse;
 import com.dashboard.funds.services.FundService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class Controller {
 	
